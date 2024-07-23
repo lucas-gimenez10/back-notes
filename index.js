b0817f3da9657d321da5ce2f616b1148fac5efca
Lucas Jose Gimenez <lucas.gimenez08@gmail.com>
2024-07-23 15:21:07 -0300

    db 2

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-require('dotenv').config()
+require("dotenv").config();
 const express = require("express");
-const cors = require('cors')
+const cors = require("cors");
 const app = express();
-const Note = require('./models/note')
-// const mongoose = require("mongoose");
+const Note = require("./models/note");
 
 let notes = [
     {
@@ -23,10 +22,9 @@ let notes = [
     },
 ];
 
-
-app.use(cors())
+app.use(cors());
 app.use(express.json());
-app.use(express.static('dist'))
+app.use(express.static("dist"));
 
 const requestLogger = (request, response, next) => {
     console.log("Method:", request.method);
@@ -38,51 +36,20 @@ const requestLogger = (request, response, next) => {
 
 app.use(requestLogger);
 
-// Mongoose
-// const password = process.argv[2];
-// const password = 'Marcia156';
-
-// const url = `mongodb+srv://lucasgimenez08:${password}@test-curso.irchihr.mongodb.net/noteApp?retryWrites=true&w=majority&appName=test-curso`;
-
-// mongoose.set("strictQuery", false);
-// mongoose.connect(url);
-
-// const noteSchema = new mongoose.Schema({
-//     content: String,
-//     important: Boolean,
-// });
-
-// noteSchema.set('toJSON', {
-//     transform: (document, returnedObject) => {
-//       returnedObject.id = returnedObject._id.toString()
-//       delete returnedObject._id
-//       delete returnedObject.id
-//       delete returnedObject.__v
-//     }
-//   })
-
-// const Note = mongoose.model("Note", noteSchema);
-
-
 app.get("/", (request, response) => {
     response.send("<h1>Hello World!!!</h1>");
 });
 
-app.get('/api/notes', (request, response) => {
-    Note.find({}).then(notes => {
-      response.json(notes)
-    })
-  })
+app.get("/api/notes", (request, response) => {
+    Note.find({}).then((notes) => {
+        response.json(notes);
+    });
+});
 
 app.get("/api/notes/:id", (request, response) => {
-    const id = Number(request.params.id);
-    const note = notes.find((note) => note.id === id);
-
-    if (note) {
+    Note.findById(request.params.id).then((note) => {
         response.json(note);
-    } else {
-        response.status(404).end();
-    }
+    });
 });
 
 app.delete("/api/notes/:id", (request, response) => {
@@ -92,32 +59,22 @@ app.delete("/api/notes/:id", (request, response) => {
     response.status(204).end();
 });
 
-const generateId = () => {
-    const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
-    return maxId + 1;
-};
-
 app.post("/api/notes", (request, response) => {
     const body = request.body;
 
-    if (!body.content) {
-        return response.status(400).json({
-            error: "content missing",
-        });
+    if (body.content === undefined) {
+        return response.status(400).json({ error: "content missing" });
     }
 
-    const note = {
+    const note = new Note({
         content: body.content,
-        important: Boolean(body.important) || false,
-        id: generateId(),
-    };
+        important: body.important || false,
+    });
 
-    notes = notes.concat(note);
-
-    response.json(note);
+    note.save().then((savedNote) => {
+        response.json(savedNote);
+    });
 });
-
-
 
 const unknownEndpoint = (request, response) => {
     response.status(404).send({ error: "unknown endpoint" });
@@ -125,7 +82,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint);
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
